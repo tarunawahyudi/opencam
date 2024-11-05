df4130f07bb2f644bb0ae136efcde367260aa533
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-11-06 03:38:40 +0700

    disable eslint

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -11,7 +11,7 @@ import {useParams} from "next/navigation";
 
 const Meeting = () => {
   const { id } = useParams();
-  const { user, isLoaded } = useUser();
+  const {  isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const { call, isCallLoading } = useGetCallById(id);
 
